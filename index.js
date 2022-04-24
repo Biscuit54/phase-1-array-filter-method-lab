e3e102bc9e9c5780b4b2f908001c6ef09a2236ab
Jacob Delgadillo <jacobdelgadillo751@gmail.com>
2022-04-23 23:12:58 -0700

    commit tree 872fb02e93e511a9da18d6edb3cdea47b72e6e4f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function  findMatching(name,item){
+    const result = []
+    for(let item of name){
+        if(item === name){
+            result.push(item)
+        }
+    }
+    return result;
+}
